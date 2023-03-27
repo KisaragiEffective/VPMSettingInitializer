d09a79c05741d89b243d1f55889d99a23affaa44
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-03-27 19:40:51 +0900

    feat: add reset

---
+++ VPMSettingInitializer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VRC.PackageManagement.Core;
+
+namespace VPMSettingInitializer;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.Error.WriteLine("usage: VPMSettingInitializer [config.json path]");
+            return 1;
+        }
+        var settings = new Settings(args[0]);
+        settings.Save();
+        
+        return 0;
+    }
+}
